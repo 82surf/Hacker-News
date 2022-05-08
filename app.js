e3b937e7f7d03c6474b5fe0bfc070a9beb3edff8
82surf <82surf@gmail.com>
2022-05-08 22:35:32 +0900

    refactor: 💡 라우터 구현

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const container = document.getElementById('root');
-const content = document.createElement('div');
+const BASE_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
 ajax = new XMLHttpRequest();
 function getData(url) {
@@ -9,30 +10,39 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
-const BASE_URL = 'https://api.hnpwa.com/v0/news/1.json';
-const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
-
-window.addEventListener('hashchange', () => {
-  const id = window.location.hash.substring(1);
-  const newsDetail = getData(CONTENT_URL.replace('@id', id));
-  const h1 = document.createElement('h1');
-  h1.innerHTML = newsDetail.title;
-  content.appendChild(h1);
-});
-
-const newsList = getData(BASE_URL);
-
-const ul = document.createElement('ul');
-
-for (const news of newsList) {
-  const div = document.createElement('div');
-  div.innerHTML = `
-    <li>
-      <a href="#${news.id}">${news.title}</a>
-    </li>
-  `;
-  ul.appendChild(div.firstElementChild);
+function newsFeed() {
+  const newsList = getData(BASE_URL);
+  const newsHtml = ['<ul>'];
+  for (const news of newsList) {
+    titleEl = `
+      <li>
+        <a href="#${news.id}">${news.title}</a>
+      </li>
+    `;
+    newsHtml.push(titleEl);
+  }
+  newsHtml.push('</ul>');
+  container.innerHTML = newsHtml.join('');
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function newsDetail() {
+  const id = window.location.hash.substring(1);
+  const news = getData(CONTENT_URL.replace('@id', id));
+  container.innerHTML = `
+    <h1>${news.title}</h1>
+    <a href="#">돌아가기</a>
+  `;
+}
+
+function router() {
+  const routePath = location.hash;
+  if (routePath === '') {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+
+window.addEventListener('hashchange', router);
+
+router();
