c39ec3ed2c7847f442cd1752c385170e1e82dcaa
82surf <82surf@gmail.com>
2022-05-08 22:00:10 +0900

    feat: 🎸 기사 컨텐츠 API 연결

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
+const container = document.getElementById('root');
+const content = document.createElement('div');
+
 const ajax = new XMLHttpRequest();
 const BASE_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+
 ajax.open('GET', BASE_URL, false);
 ajax.send();
 
+window.addEventListener('hashchange', () => {
+  const id = window.location.hash.substring(1);
+  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+  ajax.send();
+  const newsDetail = JSON.parse(ajax.response);
+  const h1 = document.createElement('h1');
+  h1.innerHTML = newsDetail.title;
+  content.appendChild(h1);
+});
+
 const newsList = JSON.parse(ajax.response);
 
-const root = document.getElementById('root');
 const ul = document.createElement('ul');
 
 for (const news of newsList) {
   const li = document.createElement('li');
-  li.innerHTML = news.title;
+  const a = document.createElement('a');
+  a.href = `#${news.id}`;
+  a.innerHTML = news.title;
+  li.appendChild(a);
   ul.appendChild(li);
 }
 
-root.appendChild(ul);
+container.appendChild(ul);
+container.appendChild(content);
